797e5d9d3bd245c182a3e42091a7e53b49f1fc2a
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-07-01 20:37:06 +0200

    tg

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  profileForm = new FormGroup({
+    username: new FormControl(' '),
+    name: new FormControl(' '),
+    surname: new FormControl(' '),
+    dob: new FormControl(' '),
+    email: new FormControl(' '),
+    password: new FormControl(' '),
+    address: new FormControl(' '),
+    city: new FormControl(' '),
+    cardNumber: new FormControl(' '),
+  })
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    this.registerObjectToArray();
+  }
+
+
+  registerObjectToArray(){
+    type MyRegisterResponse = { [key: string]: User };
+
+    let result = Object.entries(this.profileForm.value as MyRegisterResponse).map(([key, value]) => {
+      return {
+        ...value,
+        id: key,
+      }
+    });
+    console.log(result);
+  }
+
 
 }
